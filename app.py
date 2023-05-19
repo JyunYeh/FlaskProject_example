868f31d0dee6ffc19be6ccc83fdd54e0f01502fe
JyunYeh <jyunnyeh@gmail.com>
2023-05-19 17:06:04 +0800

    update 0519

--- app.py
+++ app.py
@@ -4,7 +4,11 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return render_template('index.html')
+    # jinja 2 example:
+    list = [1,2,3,4,5]
+    return render_template('index.html', list = list)
+
+
 
 if __name__ == '__main__':
     app.run()
